d1a9728cf383faa7d26ea051d82707ab658d044b
Pablo Hinojosa López <pablo.hinojosa.lopez@gmv.com>
2022-07-22 01:51:57 +0200

    Add application to send file to remote client

--- ClientApplication/src/ClientApplication.cpp
+++ ClientApplication/src/ClientApplication.cpp
@@ -8,6 +8,9 @@
 
 #include <iostream>
 #include <winsock2.h>
+#include <fstream>
+#include <ostream>
+#include <istream>
 
 using namespace std;
 
@@ -16,38 +19,134 @@ public:
     WSADATA WSAData;
     SOCKET server;
     SOCKADDR_IN addr;
-    char buffer[1024];
+    int iChunkSize = 4 * 1024; // by default
+    char cBuffer[5000];
+
+    /* Constructor of the class */
     Client()
     {
-        cout<<"Conectando al servidor..."<<endl<<endl;
+        cout<<"Connecting to server..."<<endl<<endl;
         WSAStartup(MAKEWORD(2,0), &WSAData);
         server = socket(AF_INET, SOCK_STREAM, 0);
         addr.sin_addr.s_addr = inet_addr("192.168.43.102");
         addr.sin_family = AF_INET;
         addr.sin_port = htons(5555);
         connect(server, (SOCKADDR *)&addr, sizeof(addr));
-        cout << "Conectado al Servidor!" << endl;
+        cout << "Server connected!" << endl;
     }
-    void Enviar()
+
+    /* Receives data in to buffer until bufferSize value is met */
+    int RecvBuffer(char* buffer, int bufferSize, int chunkSize = 4 * 1024) {
+        int i = 0;
+        while (i < bufferSize) {
+            const int l = recv(server, &buffer[i], __min(chunkSize, bufferSize - i), 0);
+            if (l < 0) { return l; } // this is an error
+            i += l;
+        }
+        return i;
+    }
+
+    /* Sends data in buffer until bufferSize value is met */
+    int SendBuffer(const char* buffer, int bufferSize, int chunkSize = 4 * 1024) {
+
+        int i = 0;
+        while (i < bufferSize) {
+            const int l = send(server, &buffer[i], __min(chunkSize, bufferSize - i), 0);
+            if (l < 0) { return l; } // this is an error
+            i += l;
+        }
+        return i;
+    }
+
+    /* Sends a file
+	returns size of file if success
+	returns -1 if file couldn't be opened for input
+	returns -2 if couldn't send file length properly
+	returns -3 if file couldn't be sent properly */
+    int64_t SendFile(const std::string& fileName, int chunkSize = 64 * 1024) {
+
+        const int64_t fileSize = GetFileSize(fileName);
+        if (fileSize < 0) { return -1; }
+
+        std::ifstream file(fileName, std::ifstream::binary);
+        if (file.fail()) { return -1; }
+
+        /* Send FileSize */
+        if (SendBuffer(reinterpret_cast<const char*>(&fileSize),
+            sizeof(fileSize)) != sizeof(fileSize)) {
+            return -2;
+        }
+
+        char* buffer = new char[chunkSize];
+        bool errored = false;
+        int64_t i = fileSize;
+        while (i != 0) {
+            const int64_t ssize = __min(i, (int64_t)chunkSize);
+            if (!file.read(buffer, ssize)) { errored = true; break; }
+            const int l = SendBuffer(buffer, (int)ssize);
+            if (l < 0) { errored = true; break; }
+            i -= l;
+        }
+        delete[] buffer;
+
+        file.close();
+
+        return errored ? -3 : fileSize;
+    }
+
+    /* Receives a file
+	returns size of file if success
+	returns -1 if file couldn't be opened for output
+	returns -2 if couldn't receive file length properly
+	returns -3 if couldn't receive file properly */
+    int64_t RecvFile(const std::string& fileName, int chunkSize = 64 * 1024)
     {
-        cout<<"Escribe el mensaje a enviar: ";
-        cin>>this->buffer;
-        send(server, buffer, sizeof(buffer), 0);
-        memset(buffer, 0, sizeof(buffer));
-        cout << "Mensaje enviado!" << endl;
+        std::ofstream file(fileName, std::ofstream::binary);
+        if (file.fail()) { return -1; }
+
+        /* Receive file size */
+        int64_t fileSize;
+        if (RecvBuffer(reinterpret_cast<char*>(&fileSize),
+                sizeof(fileSize)) != sizeof(fileSize)) {
+            return -2;
+        }
+
+        char* buffer = new char[chunkSize];
+        bool errored = false;
+        int64_t i = fileSize;
+        while (i != 0) {
+            const int r = RecvBuffer(buffer, (int)__min(i, (int64_t)chunkSize));
+            if ((r < 0) || !file.write(buffer, r)) { errored = true; break; }
+            i -= r;
+        }
+        delete[] buffer;
+
+        file.close();
+
+        return errored ? -3 : fileSize;
     }
-    void Recibir()
-    {
-        recv(server, buffer, sizeof(buffer), 0);
-        cout << "El servidor dice: " << buffer << endl;
-        memset(buffer, 0, sizeof(buffer));
-    }
-    void CerrarSocket()
+
+    /* Close the socket */
+    void CloseSocket()
     {
        closesocket(server);
        WSACleanup();
-       cout << "Socket cerrado." << endl << endl;
+       cout << "Socket closed." << endl << endl;
     }
+
+private:
+
+    int64_t GetFileSize(const std::string& fileName)
+    {
+		FILE* f;
+		if (fopen_s(&f, fileName.c_str(), "rb") != 0) {
+			return -1;
+		}
+		_fseeki64(f, 0, SEEK_END);
+		const int64_t len = _ftelli64(f);
+		fclose(f);
+		return len;
+	}
 };
 
 int main()
@@ -55,7 +154,6 @@ int main()
     Client *Cliente = new Client();
     while(true)
     {
-        Cliente->Enviar();
-        Cliente->Recibir();
+        Cliente->RecvFile("ReceivedData.txt");
     }
 }
--- ServerApplication/src/ServerApplication.cpp
+++ ServerApplication/src/ServerApplication.cpp
@@ -6,8 +6,12 @@
 // Description : Application to manage TCP/IP server functions.
 //============================================================================
 
+/* System includes */
 #include <iostream>
 #include <winsock2.h>
+#include <fstream>
+#include <ostream>
+#include <istream>
 
 using namespace std;
 
@@ -16,46 +20,141 @@ public:
     WSADATA WSAData;
     SOCKET server, client;
     SOCKADDR_IN serverAddr, clientAddr;
-    char buffer[1024];
+    int iChunkSize  = 4 * 1024; // by default
+    char cBuffer[5000];
+
+    /* Constructor of the class */
     Server()
     {
         WSAStartup(MAKEWORD(2,0), &WSAData);
         server = socket(AF_INET, SOCK_STREAM, 0);
 
+        /* Stablish server Characteristics */
         serverAddr.sin_addr.s_addr = INADDR_ANY;
         serverAddr.sin_family = AF_INET;
         serverAddr.sin_port = htons(5555);
 
+        /* Bind to make the socket listen to new clients */
         bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
         listen(server, 0);
 
-        cout << "Escuchando para conexiones entrantes." << endl;
+        cout << "Listening to entry connections." << endl;
         int clientAddrSize = sizeof(clientAddr);
         if((client = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
         {
-            cout << "Cliente conectado!" << endl;
+            cout << "Client connected!" << endl;
         }
     }
 
-    string Recibir()
-    {
-      recv(client, buffer, sizeof(buffer), 0);
-      cout << "El cliente dice: " << buffer << endl;
-      memset(buffer, 0, sizeof(buffer));
-      return buffer;
+    /* Receives data in to buffer until bufferSize value is met */
+    int RecvBuffer(char* buffer, int bufferSize, int chunkSize = 4 * 1024) {
+        int i = 0;
+        while (i < bufferSize) {
+            const int l = recv(client, &buffer[i], __min(chunkSize, bufferSize - i), 0);
+            if (l < 0) { return l; } // this is an error
+            i += l;
+        }
+        return i;
     }
-    void Enviar()
-    {
-        cout<<"Escribe el mensaje a enviar: ";
-        cin>>this->buffer;
-        send(client, buffer, sizeof(buffer), 0);
-        memset(buffer, 0, sizeof(buffer));
-        cout << "Mensaje enviado!" << endl;
+
+    /* Sends data in buffer until bufferSize value is met */
+    int SendBuffer(const char* buffer, int bufferSize, int chunkSize = 4 * 1024) {
+
+        int i = 0;
+        while (i < bufferSize) {
+            const int l = send(client, &buffer[i], __min(chunkSize, bufferSize - i), 0);
+            if (l < 0) { return l; } // this is an error
+            i += l;
+        }
+        return i;
     }
-    void CerrarSocket()
+
+    /* Sends a file
+    returns size of file if success
+    returns -1 if file couldn't be opened for input
+    returns -2 if couldn't send file length properly
+    returns -3 if file couldn't be sent properly */
+    int64_t SendFile(const std::string& fileName, int chunkSize = 64 * 1024) {
+
+        const int64_t fileSize = GetFileSize(fileName);
+        if (fileSize < 0) { return -1; }
+
+        std::ifstream file(fileName, std::ifstream::binary);
+        if (file.fail()) { return -1; }
+
+        /* Send FileSize */
+        if (SendBuffer(reinterpret_cast<const char*>(&fileSize),
+            sizeof(fileSize)) != sizeof(fileSize)) {
+            return -2;
+        }
+
+        /* Send Data */
+        char* buffer = new char[chunkSize];
+        bool errored = false;
+        int64_t i = fileSize;
+        while (i != 0) {
+            const int64_t ssize = __min(i, (int64_t)chunkSize);
+            if (!file.read(buffer, ssize)) { errored = true; break; }
+            const int l = SendBuffer(buffer, (int)ssize);
+            if (l < 0) { errored = true; break; }
+            i -= l;
+        }
+        delete[] buffer;
+
+        file.close();
+
+        return errored ? -3 : fileSize;
+    }
+
+    /* Receives a file
+    returns size of file if success
+    returns -1 if file couldn't be opened for output
+    returns -2 if couldn't receive file length properly
+    returns -3 if couldn't receive file properly */
+    int64_t RecvFile(const std::string& fileName, int chunkSize = 64 * 1024)
+    {
+        std::ofstream file(fileName, std::ofstream::binary);
+        if (file.fail()) { return -1; }
+
+        /* Receive file size */
+        int64_t fileSize;
+        if (RecvBuffer(reinterpret_cast<char*>(&fileSize),
+                sizeof(fileSize)) != sizeof(fileSize)) {
+            return -2;
+        }
+
+        char* buffer = new char[chunkSize];
+        bool errored = false;
+        int64_t i = fileSize;
+        while (i != 0) {
+            const int r = RecvBuffer(buffer, (int)__min(i, (int64_t)chunkSize));
+            if ((r < 0) || !file.write(buffer, r)) { errored = true; break; }
+            i -= r;
+        }
+        delete[] buffer;
+
+        file.close();
+
+        return errored ? -3 : fileSize;
+    }
+
+    /* Close the socket */
+    void CloseSocket()
     {
         closesocket(client);
-        cout << "Socket cerrado, cliente desconectado." << endl;
+        cout << "Socket closed, client disconnected." << endl;
+    }
+private:
+
+    int64_t GetFileSize(const std::string& fileName) {
+        FILE* f;
+        if (fopen_s(&f, fileName.c_str(), "rb") != 0) {
+            return -1;
+        }
+        _fseeki64(f, 0, SEEK_END);
+        const int64_t len = _ftelli64(f);
+        fclose(f);
+        return len;
     }
 };
 
@@ -63,9 +162,11 @@ public:
 int main()
 {
   Server *Servidor = new Server();
-  while(true)
+  int64_t SendFile = -1;
+  int iNumberOfTries = 10;
+  while(iNumberOfTries > 0  || SendFile > 0)
   {
-     Servidor->Recibir();
-     Servidor->Enviar();
+	  SendFile = Servidor->SendFile("SendData.txt");
+	  iNumberOfTries -= 1;
   }
 }
